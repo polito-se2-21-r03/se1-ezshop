83995491235c4fdc8e6a071680466195687bb772
Simone Alberto Peirone <s286886@studenti.polito.it>
2021-05-17 09:42:22 +0200

    Improve tests on JsonInterface

--- src/test/java/unitTests/TestJsonInterface.java
+++ src/test/java/unitTests/TestJsonInterface.java
@@ -18,14 +18,18 @@ public class TestJsonInterface {
     public static final ProductType product = new ProductType("xx", "xx", "xx", 10.0, 1);
 
     private static final String dataDirectory = "tmp/";
-    JsonInterface ji;
+
+    private JsonInterface ji;
 
     @Before
     public void clean() throws IOException {
         ji = JsonInterface.create(dataDirectory);
-        //ji.reset();
+        ji.reset();
     }
 
+    /**
+     * Test reading and writing of a list of users.
+     */
     @Test
     public void testReadWriteUsers() throws IOException {
         // write a null list
@@ -48,6 +52,9 @@ public class TestJsonInterface {
         assertTrue(readData.containsAll(writeData));
     }
 
+    /**
+     * Test reading and writing of a list of products.
+     */
     @Test
     public void testReadWriteProducts() throws IOException {
         // write a null list
@@ -75,6 +82,9 @@ public class TestJsonInterface {
         assertTrue(readData.containsAll(writeData));
     }
 
+    /**
+     * Test reading and writing of a list of customers
+     */
     @Test
     public void testReadWriteCustomers() throws IOException {
         // write a null list
@@ -97,31 +107,40 @@ public class TestJsonInterface {
         assertTrue(readData.containsAll(writeData));
     }
 
+    /**
+     * Test reading and writing of an account book
+     */
     @Test
     public void testReadWriteAccountBook() throws IOException {
-        // write a null list
+        // write a null list to the persistence layer
         ji.writeAccountBook(null);
         AccountBook readData = ji.readAccountBook();
+        // read the data from the persistence layer
         assertNotNull(readData);
         assertEquals(0.0, readData.getBalance(), TestHelpers.DOUBLE_COMPARISON_THRESHOLD);
         assertEquals(0, readData.getAllTransactions().size());
 
-        // write an accountBook
+        // generate a new account book
         AccountBook writeData = new AccountBook();
 
+        // add a sale transaction to the account book
         SaleTransaction s1 = new SaleTransaction(1, LocalDate.now());
         s1.addSaleTransactionItem(product, 10, product.getPricePerUnit(), 0.0);
-
         writeData.addTransaction(s1);
         writeData.setTransactionStatus(1, OperationStatus.COMPLETED);
 
+        // add a return transaction to the account book
         ReturnTransaction r1 = new ReturnTransaction(2, s1.getBalanceId(), LocalDate.now());
         r1.addReturnTransactionItem(product, 2, product.getPricePerUnit());
         writeData.addTransaction(r1);
         writeData.setTransactionStatus(2, OperationStatus.COMPLETED);
 
-        writeData.addTransaction(s1);
-        writeData.addTransaction(r1);
+        // add credits and debits to the account book
+        Credit credit = new Credit(3, LocalDate.now(), 10.0, OperationStatus.COMPLETED);
+        Debit debit = new Debit(4, LocalDate.now(), 20.0, OperationStatus.COMPLETED);
+        writeData.addTransaction(credit);
+        writeData.addTransaction(debit);
+
         ji.writeAccountBook(writeData);
 
         // read an accountBook
